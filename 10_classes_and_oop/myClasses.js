f5e4b7e9e79515ee510cad2e434463c62a474ecc
Safwan Shaikh <75407909+safwan3848@users.noreply.github.com>
2023-10-16 05:33:45 +0000

    classes in js

---
+++ 10_classes_and_oop/myClasses.js
@@ -0,0 +1,40 @@
+class User {
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+
+  encryptPassword() {
+    return `${this.password} abc`;
+  }
+
+  changeusername() {
+    return `${this.username.toUpperCase()}`;
+  }
+}
+
+const chai = new User("safwan", "safwan@gmail.com", "1234");
+
+console.log(chai.encryptPassword());
+console.log(chai.changeusername());
+
+// behind the scene
+
+function User(username, email, password) {
+  this.username = username;
+  this.email = email;
+  this.password = password;
+}
+
+User.prototype.encryptPassword = function () {
+  return `${this.password}abc`;
+};
+User.prototype.changeUsername = function () {
+  return `${this.username.toUpperCase()}`;
+};
+
+const tea = new User("tea", "tea@gmail.com", "123");
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
