d8071d36e3ff2c925367f1b1f0caf1dabc0e8451
Safwan Shaikh <75407909+safwan3848@users.noreply.github.com>
2023-10-16 05:41:31 +0000

    Inheritance in js

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,28 @@
+class User {
+  constructor(username) {
+    this.username = username;
+  }
+  logMe() {
+    console.log(`username is ${this.username}`);
+  }
+}
+
+class Teacher extends User {
+  constructor(username, email, password) {
+    super(username);
+    this.email = email;
+    this.password = password;
+  }
+
+  addCourse() {
+    console.log(`A new course was added by ${this.username}`);
+  }
+}
+
+const chai = new Teacher("chai", "test@gmail.com", "123");
+chai.addCourse();
+const masalaChai = new User("test");
+
+masalaChai.logMe();
+
+console.log(chai instanceof User);
